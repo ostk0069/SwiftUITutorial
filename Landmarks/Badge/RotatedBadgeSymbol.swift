ceacccccefebc591e09fe40744e9c5838c61c9da
Takuma Osada <judgment9ttk069@gmail.com>
2019-06-09 17:00:10 +0900

    playing with graph

---
+++ Landmarks/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Takuma Osada on 2019/06/09.
+//  Copyright © 2019 Takuma Osada. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
